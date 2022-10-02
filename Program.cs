608562e5c48c225f96a6e7a649e8f7c4e96e68ef
Igor <igor_parygin@mail.ru>
2022-10-02 21:35:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу,
+// которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите ряд чисел, разделенных запятой : ");
+string? numbers = Console.ReadLine();
+int B = numbers.Length;
+int[] array = new int[B] ;
+int i = 0;
+while(i < B)
+{
+    if (numbers[i] == ',') {}
+    else {
+        while(i == ',')
+        {
+            int [] mas = nambers[i];
+            i++;
+        }
+        x = mas
+    }
+    array[i] = Convert.ToInt32(numbers[i]);
+    Console.Write(numbers[i]);
+    i = i + 1;
+}
